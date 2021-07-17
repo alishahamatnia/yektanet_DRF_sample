4a086ad94cd25aec225bcc5e8781f269398bfa42
Ali Shahamatnia <ali.shahamatnia@gmail.com>
2021-07-17 05:25:50 +0430

    reports (first two parts)

--- advertising_management/urls.py
+++ advertising_management/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('ads/', AdViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('ads/<int:pk>/', AdViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
     path('ads/<int:pk>/click', ClickCreateAPIView.as_view()),
-
+    path('ads/report', ReportApiView.as_view()),
 
 ]
--- advertising_management/views.py
+++ advertising_management/views.py
@@ -1,14 +1,17 @@
 import datetime
-import time
+from collections import defaultdict
 
+from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
-from rest_framework.generics import CreateAPIView, ListCreateAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
-from .models import Ad
+from .models import Ad, Seen, Click
 from .serializers import AdSCreateSerializer, AdListSerializer, CLickSerializer, SeenSerializer
 
 
@@ -47,6 +50,53 @@ class ClickCreateAPIView(CreateAPIView):
         serializer.save(involved_ad=get_object_or_404(Ad, pk=self.kwargs['pk']),
                         impression_time=datetime.datetime.now(),
                         user_ip=get_client_ip(self.request))
+
+
+class ReportApiView(APIView):
+
+    def get(self, *args, **kwargs):
+        clicks_report = Click.objects.values_list(
+            'involved_ad',
+            'impression_time',
+            'user_ip',
+        ).annotate(count=Count('id')).order_by('involved_ad', 'user_ip','impression_time')
+        views_report = Seen.objects.values_list(
+            'involved_ad',
+            'impression_time',
+            'user_ip',
+        ).annotate(count=Count('id')).order_by('involved_ad', 'user_ip','impression_time')
+
+        impression_report = defaultdict(lambda: defaultdict(lambda: {
+            'clicked': 0,
+            'viewed': 0
+        }))
+
+        impression_report_per_date = defaultdict(lambda: {
+            'clicked': 0,
+            'viewed': 0
+        })
+
+        total_clicked = 0
+        total_viewed = 0
+        for ad_id, impression_time, _, clicked in clicks_report:
+            impression_report[ad_id][impression_time.strftime('%Y-%m-%dT%H')]['clicked'] += clicked
+            impression_report_per_date[impression_time.strftime('%Y-%m-%dT%H')]['clicked'] += clicked
+            total_clicked += clicked
+        for ad_id, impression_time, _, viewed in views_report:
+            impression_report[ad_id][impression_time.strftime('%Y-%m-%dT%H')]['viewed'] += viewed
+            impression_report_per_date[impression_time.strftime('%Y-%m-%dT%H')]['viewed'] += viewed
+            total_viewed += viewed
+
+        click_per_view_report = dict(total=total_clicked / total_viewed if total_viewed else 1)
+        for impression_date, impression_data in impression_report_per_date.items():
+            click_per_view_report[impression_date] = impression_data['clicked'] / impression_data['viewed'] \
+                if impression_data['viewed'] else 1
+
+        clicked_viewd_average_time  =
+        return Response({
+            'impression_report': impression_report,
+            'click_per_view_report': click_per_view_report,
+        })
 
 
 def get_client_ip(request):
